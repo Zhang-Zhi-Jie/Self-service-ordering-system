66daa166687dd93d941eddf5e23cc000da97e8aa
zzj12311 <1305144350@qq.com>
2016-05-10 18:42:58 +0800

    12345

---
+++ app/src/main/java/com/example/a13051_000/buffetmealsystem/FragmentAt.java
@@ -0,0 +1,19 @@
+package com.example.a13051_000.buffetmealsystem;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by 13051_000 on 2016/5/10.
+ */
+public class FragmentAt  extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.activity_main, container, false);
+    }
+}
+
