91ee20bd06cb8ed544ff03058938d6113688d7db
zzj12311 <1305144350@qq.com>
2016-06-09 15:04:25 +0800

    界面大换血

--- app/src/main/java/com/example/a13051_000/buffetmealsystem/LoginActivity.java
+++ app/src/main/java/com/example/a13051_000/buffetmealsystem/LoginActivity.java
@@ -4,8 +4,6 @@ import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Message;
-import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -15,94 +13,96 @@ import android.widget.Toast;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.logging.Handler;
-import java.util.logging.LogRecord;
-
 
 /**
- * Created by 13051_000 on 2016/4/26.
- * 用多线程重写加载函数2016/02/15
+ * Created by 13051_000 on 2016/4/20.
  */
 public class LoginActivity extends BaseActivity {
-    private ProgressDialog progressDialog;
-    private Button button;
-<<<<<<< HEAD
-    private String strResult;
     public static final int SHOW_RESPONSE = 0;
-    private android.os.Handler handler = new android.os.Handler() {
-        public void handleMessage(Message message) {
-            switch (message.what) {
+    private Button button1;
+    private TextView textView1;
+    private static String sResult = "";
+    private ProgressDialog progressDialog;
+    private String number;
+    private String pwd;
+    //异步消息处理；；；
+    private android.os.Handler handler = new android.os.Handler(){
+        public void handleMessage(Message message){
+            switch (message.what){
                 case SHOW_RESPONSE:
-                    String result = Json.parseJSONWithJOSNObject(strResult);
-                    progressDialog.dismiss();
-                    Toast.makeText(LoginActivity.this, "注册成功!", Toast.LENGTH_SHORT).show();
-                    Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
-                    startActivity(intent);
+                    String response = (String) message.obj;
+                    sResult = response;
+                    if (!(sResult == "")) {
+                        String result = "";
+                        try {
+                            result = Json.parseJSONWithJOSNObject(sResult);
+                        } catch (Exception e) {
+                            Log.d("data1", e.toString());
+                        }
+                        progressDialog.dismiss();
+                        if (!result.equals("null") && !(result == "")) {
+                            Toast.makeText(getApplicationContext(), "您的用户名为:" + Json.parseJSONWithJOSNObject(sResult), Toast.LENGTH_SHORT).show();
+                            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                            LoginActivity.this.startActivity(intent);
+                        } else {
+                            Toast.makeText(getApplicationContext(), "信息输入错误,请重新输入", Toast.LENGTH_SHORT).show();
+                        }
+                    }
             }
         }
     };
-=======
-    private Button rightbutton;
->>>>>>> zzj/master
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
 
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        Topbar topbar= (Topbar) findViewById(R.id.topbar1);
-        topbar.setOnTobarClickListener(new Topbar.topbarClickListener() {
-            @Override
-            public void leftClick() {
-
-            }
-
-            @Override
-            public void rightClick() {
-               Intent intent=new Intent(LoginActivity.this,RegisterActivity.class);
-                LoginActivity.this.startActivity(intent);
-            }
-        });
-        button= (Button) findViewById(R.id.loginbutton1);
-        button.setOnClickListener(new View.OnClickListener() {
+        button1= (Button) findViewById(R.id.button1);
+        textView1= (TextView) findViewById(R.id.textView1);
+        textView1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String loginnickname=((EditText)findViewById(R.id.logineditText4)).getText().toString();
-                String loginnum=((EditText)findViewById(R.id.logineditText1)).getText().toString();
-                String loginpwd=((EditText)findViewById(R.id.logineditText2)).getText().toString();
-                String againpwd=((EditText)findViewById(R.id.logineditText3)).getText().toString();
-                boolean login=false;
-                if(loginpwd.equals(againpwd)){
-                    login = true;
-                }
-                if(login) {
-                    Map<String, String> params = new HashMap<String, String>();
-                    params.put("user", loginnum);
-                    params.put("password", loginpwd);
-                    params.put("nickname", loginnickname);
-                    String strUrlPath = "http://www.loushubin.cn/register.php";
-                    sendRequest(strUrlPath,params);
-                    progressDialog = new ProgressDialog(LoginActivity.this);
-                    progressDialog.setTitle("正在加载...");
-                    progressDialog.setMessage("Loading...");
-                    progressDialog.setCancelable(true);
-                    progressDialog.show();
-                }
-                    else{
-                        Toast.makeText(getApplicationContext(),"密码输入不匹配，请重新输入",Toast.LENGTH_SHORT).show();
-                    }
-
+                Intent intent1=new Intent(LoginActivity.this,RegisterActivity.class);
+                LoginActivity.this.startActivity(intent1);
             }
         });
-    }
-    private void sendRequest(final String strUrlPath,final Map<String,String> params){
-        new Thread(new Runnable() {
+        button1.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void run() {
-                strResult = HttpUtils.submitPostData(strUrlPath, params, "utf-8");
-                Message message = new Message();
-                message.what = SHOW_RESPONSE;
-                handler.sendMessage(message);
+            public void onClick(View view) {
+                //启动等待活动
+                switch (view.getId()) {
+                    case R.id.button1:
+                        progressDialog = new ProgressDialog(LoginActivity.this);
+                        progressDialog.setTitle("正在加载...");
+                        progressDialog.setMessage("Loading...");
+                        progressDialog.setCancelable(true);
+                        progressDialog.show();
+                        Log.d("data1","Result:"+sResult);
+                        //将数据加入请求当中
+                        number = ((EditText) findViewById(R.id.editText1)).getText().toString();
+                        pwd = ((EditText) findViewById(R.id.editText2)).getText().toString();
+                        boolean flag = false;
+                        String nickname = "";
+                        Map<String, String> params = new HashMap<String, String>();
+                        params.put("user", number);
+                        params.put("password", pwd);
+                        String strUrlPath = "http://www.loushubin.cn/login_user.php";
+                        //调用Thread，创建新线程进行网络请求
+                        sendRequest(strUrlPath, params);
+                }
             }
-        }).start();
-    }
-
+            //启动新的线程
+            private void sendRequest(final String strUrlPath, final Map<String,String> params){
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        String strResult =HttpUtils.submitPostData(strUrlPath,params,"utf-8");
+                        Message message = new Message();
+                        message.what = SHOW_RESPONSE;
+                        message.obj = strResult;
+                        handler.sendMessage(message);
+                    }
+                }).start();
+            }
+        });
+}
 }
