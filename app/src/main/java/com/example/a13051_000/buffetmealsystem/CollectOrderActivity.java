c525b598acfbb3aa4a98f61790ec5bc4a4bcfe34
zzj12311 <1305144350@qq.com>
2016-06-12 22:09:56 +0800

    新增一些布局、欢迎界面，以后继续修改

---
+++ app/src/main/java/com/example/a13051_000/buffetmealsystem/CollectOrderActivity.java
@@ -0,0 +1,25 @@
+package com.example.a13051_000.buffetmealsystem;
+
+import android.app.ProgressDialog;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+/**
+ * Created by 13051_000 on 2016/6/12.
+ */
+public class CollectOrderActivity extends AppCompatActivity{
+    private ListView collectList;
+    private TextView tv_bg;
+    private String str;
+    private Cursor cursor;
+    protected  void onCreated(Bundle savedInstanceState){
+       super.onCreate(savedInstanceState);
+       setContentView(R.layout.activity_collect);
+   }
+
+
+}
---
+++ app/src/main/java/com/example/a13051_000/buffetmealsystem/WelcomeActivity.java
@@ -0,0 +1,38 @@
+package com.example.a13051_000.buffetmealsystem;
+
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+/**
+ * Created by 13051_000 on 2016/6/12.
+ */
+public class WelcomeActivity extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+
+        PackageManager pm = getPackageManager();
+        try {
+            PackageInfo pi = pm.getPackageInfo("com.lyt.android", 0);
+            TextView versionNumber = (TextView) findViewById(R.id.versionNumber);
+            versionNumber.setText("Version " + pi.versionName);
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        new Handler().postDelayed(new Runnable(){
+
+            @Override
+            public void run() {
+                Intent intent = new Intent(WelcomeActivity.this,LoginActivity.class);
+                startActivity(intent);
+               WelcomeActivity.this.finish();
+            }
+        }, 3500);
+    }
+}
